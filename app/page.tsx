5e46546221f73fb7dc5b28610a36e47ad87f3492
bakxriyev <kamronbekbahriyev18@gmail.com>
2025-11-01 00:16:09 +0500

    dsd

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation"
 
 export default function HomePage() {
   // Kiruvchi har bir foydalanuvchini /c sahifasiga yo‘naltiramiz
-  redirect("/c")
+  redirect("/b")
 }
